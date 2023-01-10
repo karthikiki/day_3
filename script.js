55e27825b5acc21740b9d350aa3a52414c2c614e
Karthick C <82097946+karthikiki@users.noreply.github.com>
2023-01-10 22:49:35 +0530

    Day_3

---
+++ script.js
@@ -0,0 +1,44 @@
+let myResume={
+    "basics": {
+      "name": "Karthick C",
+      "email": "karthickcks003@gmail.com",
+      "phone": 7639034103,
+      "degree": "B.E CSE ",
+      "location": {
+        "Address": "Singaperumal koil, Chengalpattu",
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+    },
+    "education": [
+      {
+        "institution": "Vels Institute of Science, Technology & Advanced Studies (VISTAS)",
+        "department": "Computer Science Engineering",
+        "batch start year": 2018,
+        "batch end year": 2021,
+        "gpa": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript,HTML,CSS",
+        "level": "beginner",
+        "project": [
+          "Object detection using Deep Learning"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Cricket , Football",
+      }
+    ]
+  }
+  console.log(myResume);
+  
